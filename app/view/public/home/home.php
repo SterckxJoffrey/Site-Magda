d58059f7da7c2ecf64eea9cff46b9ffd881f9228
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2025-07-12 17:54:09 +0200

    home page all section + css

--- app/view/public/home/home.php
+++ app/view/public/home/home.php
@@ -1,89 +1,38 @@
 <section class="hero-section">
   <h1>Magdalena Tyminska</h1>
+  <h2>Avocat au barreau de Bruxelles</h2>
+  <p>Exerce dans tous les domaines du droit familial
+  Vous accompagne dans toutes vos démarches amiables ou judiciaires</p>
+  <a href="/contact" rel="noopener noreferrer">Contact</a>
 </section>
+<section class="description">
+<h2>Description</h2>
+<img src="/asset/img/image.webp" alt="image de moi">
+<p>Maître Magdalena TYMINSKA est avocate indépendante à Bruxelles, offrant une expertise variée en Droit de la Famille, Droit de Roulage et Permis de conduire, Droit Civil, Divorce, Droit Pénal, Baux Commerciaux, Droit des Mineurs, et Droit de l’Immobilier.</p>
+<p>Sa personnalité fiable et professionnelle, ainsi que son écoute attentive, font d’elle une avocate de confiance. Chaque client est unique et important pour Maître TYMINSKA, qui s’efforce de répondre rapidement à toutes les demandes et de défendre au mieux vos intérêts.</p>
+<p>Master en droit à finalité civile et pénale</p>
+</section>
+
 <section class="domain">
-<h2>Domaine de droit</h2>
+    <h2>Domaines d'activité</h2>
+    <p>Droit de la famille</p>
+    <p>Droit de Roulage et Permis de conduire</p>
+    <p>Droit civil</p>
+    <p>Divorce</p>
+    <p>Droit Pénal</p>
+    <p>Baux Commerciaux</p>
+    <p>Droit des Mineurs</p>
+    <p>Droit de l’Immobilier</p>
 </section>
 
-<p>test</p>
+<section class="contact">
+  <h2>Prendre rendez-vous</h2>
+  <form class="contact_form" method="POST" action="contact.php">
+    <input type="text" name="name" id="name" placeholder="Nom" required>
+    <input type="email" name="email" id="email" placeholder="Email" required>
+    <textarea name="message" id="message" placeholder="Votre message..." required></textarea>
+    <button type="submit">Envoyer</button>
+  </form>
+</section>
 
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
-<p>vknjevejnvekvevkjenvre</p>
-<h1>nczmlkzckzlnkc</h1>
-<h2>ncjkzmkzevlzncz</h2>
-<h3>cnklzclkzcznkc</h3>
-<h4>c,klzc,kz,czckl</h4>
-<h5>
-    dvknldvnkflkdvndl
-</h5>
+
